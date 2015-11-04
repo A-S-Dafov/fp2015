220c4632eb80f812b07af4c387b6487fcbba5e04
A-S-Dafov <golden_4@abv.bg>
2015-11-04 17:11:13 +0200

    Create homework4.rkt

---
+++ week4/homework4.rkt
@@ -0,0 +1,35 @@
+#lang racket
+(require "prime.rkt")
+
+(define (add1 x) (+ 1 x))
+
+(define (square x) (* x x))
+
+(define (f p g h)
+  (lambda(x)
+    (if(and (p(g x)) (p(h x))) #t
+       #f)))
+
+(define (fst pair)
+  (car pair))
+
+(define (smaller x y)
+  (if(< x y) x y))
+         
+(define (snd pair)
+  (cdr pair))
+
+(define (add-frac frac1 frac2)
+  (cons (+(* (fst frac1) (snd frac2)) (* (fst frac2) (snd frac1)))  (* (snd frac1) (snd frac2))))
+
+(define (substract-frac frac1 frac2)
+  (cons (-(* (fst frac1) (snd frac2)) (* (fst frac2) (snd frac1)))  (* (snd frac1) (snd frac2))))
+
+(define (mult-frac frac1 frac2)
+  (cons (* (fst frac1) (fst frac2)) (*(snd frac1) (snd frac2))))
+
+(define (simplify-frac frac)
+  (define (helper n)
+    (if(and (= 0 (remainder(fst frac) n)) (= 0 (remainder (snd frac) n))) (cons (/ (fst frac) n) (/ (snd frac) n))
+       (helper (- n 1))))
+  (helper (smaller (fst frac) (snd frac))))
